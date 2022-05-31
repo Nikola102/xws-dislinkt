7c1b6fbb4844691d7d058077e4f015ef07b062d8
mehighlovv <zivkovic.mihajlo0809@gmail.com>
2022-05-31 16:27:11 +0200

    Fix: Connected backend
    Connected backend with frontend,  binding frontend components to backend functionality left

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,85 +11,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      profiles: [
-        {
-          id: 1,
-          name: "Marko",
-          lastname: "Markovic",
-          posts: [1, 2, 3],
-          username: "markisa99",
-        },
-        {
-          id: 2,
-          name: "Darko",
-          lastname: "Darkovic",
-          posts: [4, 5, 6],
-          username: "markisa98",
-        },
-        {
-          id: 3,
-          name: "Zarko",
-          lastname: "Zarkovic",
-          posts: [7, 8, 9],
-          username: "markisa97",
-        },
-      ],
-      posts: [
-        {
-          id: 1,
-          content: "I'm feeling good today!",
-          likes: 52,
-          comments: [
-            { userId: 2, content: "Glad to hear that!" },
-            { userId: 3, content: "Me too my g!" },
-          ],
-        },
-        {
-          id: 2,
-          content: "I'm feeling good today!",
-          likes: 50,
-          comments: [
-            { userId: 2, content: "Glad to hear that!" },
-            { userId: 3, content: "Me too my g!" },
-          ],
-        },
-        {
-          id: 3,
-          content: "I'm feeling good today!",
-          likes: 51,
-          comments: [
-            { userId: 2, content: "Glad to hear that!" },
-            { userId: 3, content: "Me too my g!" },
-          ],
-        },
-        {
-          id: 4,
-          content: "I'm feeling good today!",
-          likes: 51,
-          comments: [
-            { userId: 1, content: "Glad to hear that!" },
-            { userId: 3, content: "Me too my g!" },
-          ],
-        },
-        {
-          id: 5,
-          content: "I'm feeling good today!",
-          likes: 51,
-          comments: [
-            { userId: 1, content: "Glad to hear that!" },
-            { userId: 3, content: "Me too my g!" },
-          ],
-        },
-        {
-          id: 6,
-          content: "I'm feeling good today!",
-          likes: 51,
-          comments: [
-            { userId: 1, content: "Glad to hear that!" },
-            { userId: 3, content: "Me too my g!" },
-          ],
-        },
-      ],
+      profiles: [],
+      posts: [],
     };
   }
   render() {
